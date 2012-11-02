a242a50dce1bd6456c2c3fab18fd94253ca935d9
Rinke Hoekstra <rjhoekstr@gmail.com>
2012-11-02 14:05:50 +0100

    Moving Figshare-specific functions to separate module

---
+++ src/util/figshare.py
@@ -0,0 +1,98 @@
+'''
+Created on Nov 2, 2012
+
+@author: hoekstra
+'''
+
+import requests
+from oauth_hook import OAuthHook
+from time import time
+from urlparse import parse_qs
+from urllib import unquote
+import json
+import random
+import string
+
+OAuthHook.consumer_key = 'K9qG70PgROIg8CpZGJlGRg'
+OAuthHook.consumer_secret = '0JdZcz5pz0HwyWbeiwsviA'
+
+
+
+def get_auth_url(request):
+    """Uses the consumer key and secret to get a token and token secret for requesting authorization from Figshare
+    
+    Returns the authorization URL, or raises an exception if the response contains an error.
+    """
+    figshare_oauth_hook = OAuthHook(header_auth=True)
+    
+    response = requests.post('http://api.figshare.com/v1/pbl/oauth/request_token', hooks={'pre_request': figshare_oauth_hook})
+    
+    qs = parse_qs(response.text)
+    
+    if 'error' in qs:
+        raise Exception(qs['error'])
+    else :     
+        oauth_request_token = qs['oauth_token'][0]
+        oauth_request_token_secret = qs['oauth_token_secret'][0]
+        oauth_request_auth_url = unquote(qs['oauth_request_auth_url'][0])
+    
+        request.session['oauth_request_token'] = oauth_request_token
+        request.session['oauth_request_token_secret'] = oauth_request_token_secret
+        
+        return oauth_request_auth_url
+
+def validate_oauth_verifier(request, oauth_verifier):
+    """Retrieves an oauth access token and secret from Figshare using the oauth_verifier (pin) provided by the user. 
+    
+    Adds the oauth token and secret to the session, or raises an exception if the response is empty or contains an error.
+    """
+    oauth_request_token = request.session.get('oauth_request_token')
+    oauth_request_token_secret = request.session.get('oauth_request_token_secret')
+    
+    figshare_oauth_hook = OAuthHook(oauth_request_token, oauth_request_token_secret, header_auth=True)
+    response = requests.post('http://api.figshare.com/v1/pbl/oauth/access_token', {'oauth_verifier': oauth_verifier}, hooks={'pre_request': figshare_oauth_hook})
+    response = parse_qs(response.content)
+    
+    if response == {} :
+        raise Exception('Authorization failed')
+    elif 'error' in response :
+        raise Exception(response['error'])
+    else :
+        oauth_token = response['oauth_token'][0]
+        oauth_token_secret = response['oauth_token_secret'][0]
+        xoauth_figshare_id = response['xoauth_figshare_id'][0]
+    
+        request.session['oauth_token'] = oauth_token
+        request.session['oauth_token_secret'] = oauth_token_secret
+        request.session['xoauth_figshare_id'] = xoauth_figshare_id
+    
+    return
+
+
+def get_articles(request):
+    """Uses the oauth token and secret to obtain all articles of the authenticated user from Figshare
+    
+    Adds the results to the session under 'items', or raises an exception if the response contains an error.
+    """
+    oauth_token = request.session.get('oauth_token')
+    oauth_token_secret = request.session.get('oauth_token_secret')
+
+    oauth_hook = OAuthHook(oauth_token, oauth_token_secret, header_auth=True)
+
+    client = requests.session(hooks={'pre_request': oauth_hook})
+
+    response = client.get('http://api.figshare.com/v1/my_data/articles')
+
+    if 'error' in response :
+        raise Exception(response['error'])
+    else :
+        results = json.loads(response.content)
+        
+        request.session['items'] = results['items']
+    
+    return
+
+
+
+
+
