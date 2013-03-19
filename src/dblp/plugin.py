2433a06844b0c79f2a262ca0259c2c2f1bb29d9b
Rinke Hoekstra <rjhoekstr@gmail.com>
2013-03-19 13:01:59 +0100

    Using UNION instead of multiple SPARQL calls

--- src/dblp/plugin.py
+++ src/dblp/plugin.py
@@ -28,39 +28,53 @@ def linkup(request, article_id):
     
     if len(i['authors']) > 0 :
         
-        for author in i['authors'] :
+        first_author = i['authors'][1]
+        a_id = first_author['id']
+        a_label = first_author['full_name'].strip()
+        a_qname = 'FS{}'.format(a_id)
+        
+        q_part = "{ ?s a foaf:Agent .\n\t\t ?s foaf:name '"+a_label+"'. } "
+        
+        for author in i['authors'][2:] :
             a_id = author['id']
             a_label = author['full_name'].strip()
             a_qname = 'FS{}'.format(a_id)
             
-            q = """
-                PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-                
-                SELECT ?s 
-                WHERE {
-                    ?s a foaf:Agent .
-                    ?s foaf:name '"""+a_label+"""'.
-                }
-            """
+            q_part += "\n\t\tUNION { ?s a foaf:Agent .\n\t\t ?s foaf:name '"+a_label+"'. } "
             
-            sparql.setQuery(q)
-    
-            try :
-                results = sparql.query().convert()
-    
-                for result in results["results"]["bindings"]:
-                    match_uri = result["s"]["value"]
-                    
-                    short = re.sub('\s','_',a_label)
-                    
-                    if len(match_uri) > 61 :
-                        show = match_uri[:29] + "..." + match_uri[-29:]
-                    else :
-                        show = match_uri
-                    authors.append({'type': 'mapping', 'uri': match_uri, 'web': match_uri, 'show': show, 'short': short, 'original': a_qname})
-            except :
-                return render_to_response('message.html',{'type': 'error', 'text':"DBLP endpoint {} produced unintelligible results. Maybe it's down?".format(sparql)})
+        q = """
+            PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+            
+            SELECT ?s 
+            WHERE {
+                """ + q_part + """
+            }
+        """
         
+        sparql.setQuery(q)
+        
+        print "Set query to "+q
+    
+        try :
+            print "Calling endpoint"
+            results = sparql.query().convert()
+            print "Done"
+            
+            for result in results["results"]["bindings"]:
+                match_uri = result["s"]["value"]
+                
+                print match_uri
+                
+                short = re.sub('\s','_',a_label)
+                
+                if len(match_uri) > 61 :
+                    show = match_uri[:29] + "..." + match_uri[-29:]
+                else :
+                    show = match_uri
+                authors.append({'type': 'mapping', 'uri': match_uri, 'web': match_uri, 'show': show, 'short': short, 'original': a_qname})
+        except :
+            return render_to_response('message.html',{'type': 'error', 'text':"DBLP endpoint {} produced unintelligible results. Maybe it's down?".format(sparql)})
+    
             
     request.session.setdefault(article_id,[]).extend(authors)
     request.session.modified = True
