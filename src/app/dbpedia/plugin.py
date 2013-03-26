4a5abeed898cac82d48d3a4f4c07e4e76e1a21da
Rinke Hoekstra <rjhoekstr@gmail.com>
2013-03-26 11:47:06 +0100

    Moved DBPedia plugin code to new base plugin as well

--- src/app/dbpedia/plugin.py
+++ src/app/dbpedia/plugin.py
@@ -11,98 +11,53 @@ http://github.com/Data2Semantics/linkitup
 from flask import render_template, session
 from flask.ext.login import login_required
 
-from SPARQLWrapper import SPARQLWrapper, JSON
-
 import re
 
-
-
 from app import app
+from app.util.baseplugin import SPARQLPlugin
 
 @app.route('/dbpedia/<article_id>')
 @login_required
 def link_to_wikipedia(article_id):
     app.logger.debug("Running DBPedia plugin for article {}".format(article_id))
-    
-    items = session['items']
-    
-    sparql = SPARQLWrapper("http://live.dbpedia.org/sparql")
-    sparql.setReturnFormat(JSON)
-    
-    urls = []
 
-    i = items[article_id]
+    # Retrieve the article from the session
+    article = session['items'][article_id]
     
-    tags_and_categories = i['tags'] + i['categories']
+    # Rewrite the tags and categories of the article in a form understood by utils.baseplugin.SPARQLPlugin
+    article_items = article['tags'] + article['categories']
+    match_items = [{'id': item['id'], 
+                    'label': re.findall('^.*?\=(.*)$',item['name'])[0] if item['name'].lower().startswith('inchikey=') else item['name']} 
+                   for item in article_items]
     
-#    app.logger.debug(tags_and_categories)
-    
-    for tag in tags_and_categories :
+    try :
+        # Initialize the plugin
+        plugin = SPARQLPlugin(endpoint = "http://live.dbpedia.org/sparql",
+                          template = "dbpedia.query",
+                          rewrite_function = lambda x: re.sub('dbpedia.org/resource','en.wikipedia.org/wiki',x),
+                          id_function = lambda x: re.sub('http://dbpedia.org/resource/','',x),
+                          id_base = 'uri')
         
-        t_id = tag['id']
-        t_label = tag['name'].strip()
-        t_qname = 'FS{}'.format(t_id)
+        # Run the plugin, and retrieve matches using the default label property (rdfs:label)
+        matches = plugin.match(match_items)
+        # Run the plugin with a specific property for the InchiKeys (namespace is defined in the dbpedia.query template)
+        matches.extend(plugin.match(match_items, property="dbpprop:inchikey"))
+        
+        # Add the matches to the session
+        session.setdefault(article_id,[]).extend(matches)
+        session.modified = True
+    
+        if matches == [] :
+            matches = None
+        
+        # Return the matches
+        return render_template('urls.html',
+                               article_id = article_id, 
+                               results = [{'title':'Wikipedia','urls': matches}])
+        
+    except Exception as e:
+        return render_template( 'message.html',
+                                type = 'error', 
+                                text = e.message )
+    
 
-        if t_label.lower().startswith('inchikey=') :
-            t_match = re.findall('^.*?\=(.*)$',t_label)[0]
-            
-            q = """
-                PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-                PREFIX dbpprop: <http://dbpedia.org/property/> 
-                SELECT DISTINCT ?s
-                WHERE { 
-                    ?s dbpprop:inchikey ?label .
-                    ?label bif:contains '\""""+t_match+"""\"' .
-                    FILTER (regex(str(?label), '^"""+t_match+"""$', 'i')).
-                    FILTER (regex(str(?s), '^http://dbpedia.org/resource')).
-                    FILTER (!regex(str(?s), '^http://dbpedia.org/resource/Category:')). 
-                    FILTER (!regex(str(?s), '^http://dbpedia.org/resource/List')).
-                    FILTER (!regex(str(?s), '^http://sw.opencyc.org/')). 
-                }
-            """
-        else :
-            t_match = t_label
-            q = """
-                PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-                SELECT DISTINCT ?s
-                WHERE { 
-                    ?s rdfs:label ?label .
-                    ?label bif:contains '\""""+t_match+"""\"' .
-                    FILTER (regex(str(?label), '^"""+t_match+"""$', 'i')).
-                    FILTER (regex(str(?s), '^http://dbpedia.org/resource')).
-                    FILTER (!regex(str(?s), '^http://dbpedia.org/resource/Category:')). 
-                    FILTER (!regex(str(?s), '^http://dbpedia.org/resource/List')).
-                    FILTER (!regex(str(?s), '^http://sw.opencyc.org/')). 
-                }
-            """      
-            
-        print "Query set to ", q  
-        sparql.setQuery(q)
-        print "Calling endpoint"
-        results = sparql.query().convert()
-        print "Done"
-        for result in results["results"]["bindings"]:
-            match_uri = result["s"]["value"]
-            
-            print match_uri
-            
-            wikipedia_uri = re.sub('dbpedia.org/resource','en.wikipedia.org/wiki',match_uri)
-            
-            short = re.sub('http://dbpedia.org/resource/','',match_uri)
-            
-            if len(wikipedia_uri) > 61 :
-                show = wikipedia_uri[:29] + "..." + wikipedia_uri[-29:]
-            else :
-                show = wikipedia_uri
-            
-            urls.append({'type': 'mapping', 'uri': match_uri, 'web': wikipedia_uri, 'show': show, 'short': short, 'original': t_qname})
-            
-    session.setdefault(article_id,[]).extend(urls)
-    session.modified = True
-    
-    if urls == [] :
-        urls = None 
-        
-    return render_template('urls.html',
-                           article_id = article_id, 
-                           results = [{'title':'Wikipedia','urls': urls}])
