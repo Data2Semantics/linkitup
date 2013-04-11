104f2b07381089a7984786d1e42f28c109c1706b
Rinke Hoekstra <rjhoekstr@gmail.com>
2013-04-11 13:52:36 +0200

    Improved upload modal and implemented unchecking of links in link overviews

--- src/app/templates/article.js
+++ src/app/templates/article.js
@@ -1,51 +1,47 @@
-
-
-
-// Default: hide the modal showing the selected links. We'll only populate & show it once the user requests us to do so.
-$("#links_{{ i.article_id }}").hide();
-
-
 // Code for showing the selected links modal
 $("#show_{{ i.article_id }}").click(function(){
-    $("#links_modalbody_{{ i.article_id }}").empty(); 
-    
-    var start = "<label class='checkbox alert alert-info'><a href='";
-    var middle1 = "' target='_new'>";
-    var end = "</a></label>";
-    
-	$("#{{ i.article_id }}_form :input[type='checkbox']:checked").each( 
-			function(){
-	        	if ($(this).attr('name') != 'csrfmiddlewaretoken') {
-	          		$("#links_modalbody_{{ i.article_id }}").append(start + $(this).attr('web') + middle1 + $(this).attr('show') + end);
-	        	}
-    });
+    showSelectedLinks("{{ i.article_id }}", "#links_modalbody_{{ i.article_id }}");
     
     $("#links_{{ i.article_id }}").modal('show');
 });
 
-// Hide the 'successfully updated Figshare' info box until we've actually done so.
-$("#{{ i.article_id }}_updated_figshare").hide();
+
 
 // Download RDF Button handling
 $("#rdf_{{ i.article_id }}").click(function(){
-	$("#{{ i.article_id }}_form").prop("action","rdf/{{ i.article_id }}");
-	$("#{{ i.article_id }}_form").submit();
+    $("#{{ i.article_id }}_form").prop("action","rdf/{{ i.article_id }}");
+    $("#{{ i.article_id }}_form").submit();
+})
+
+
+$("#add_to_figshare_{{ i.article_id }}").click(function(){
+    showSelectedLinks("{{ i.article_id }}", "#selected_links_list_{{ i.article_id }}");
+    $("#figshare_upload_progress_{{ i.article_id }}").hide();
+    $("#add_to_figshare_modal_{{ i.article_id }}").modal('show');
 })
 
 // Add to Figshare button handling
-$("#figshare_{{ i.article_id }}").click(function(){
-	// Ensure that the form submission has the right target, and does not refresh the page.
-	$("#{{ i.article_id }}_form").submit( function () {    
-	    $.post(
-	     'figshare/{{ i.article_id }}',
-	      $(this).serialize(),
-	      function(data){
-	    	 $("#{{ i.article_id }}_updated_figshare").show();
-	      }
-	    );
-	    return false;   
-	}); 
-
-	$("#{{ i.article_id }}_form").submit();
+$("#do_add_to_figshare_{{ i.article_id }}").click(function(){
+    
+    // Ensure that the form submission has the right target, and does not refresh the page.
+    $("#{{ i.article_id }}_form").submit( function () {
+	
+	// Show the progress bar, and make it active 
+	$("#figshare_upload_progress_{{ i.article_id }}").toggleClass("active");
+	$("#figshare_upload_progress_{{ i.article_id }}").show();
+	
+	$.post(
+	 'figshare/{{ i.article_id }}',
+	  $(this).serialize(),
+	  function(data){
+	    // Stop the progress bar from moving, and change the text to "Success!"
+	    $("#figshare_upload_progress_{{ i.article_id }}").toggleClass("active");
+	    $("#figshare_upload_progress_{{ i.article_id }} > div").text("Success!");
+	  }
+	);
+	return false;   
+    });
+    
+    $("#{{ i.article_id }}_form").submit();
 })
 
--- src/app/templates/articles.js
+++ src/app/templates/articles.js
@@ -2,13 +2,6 @@ $.ajaxSetup({
 	cache: false  
 });
 
-// data-role functionality
-//$(function(){
-//    $("[data-role=submit]").click(function(){
-//        $(this).closest("form").submit();
-//    });
-//});
-	
 $("#articles").change(function() {
 	var article_id = $("#articles").val();
 	var article_tab = "#" + article_id + "_tab";
@@ -22,3 +15,47 @@ $("#articles").change(function() {
 	$(article_tab).show();
 	$(article_plugins).show();
 });
+
+function showSelectedLinks(articleid, element) {
+	$(element).empty();
+	
+	var start = "<label class='checkbox alert alert-info'><a href='";
+	var middle1 = "' target='_new'>";
+	var end = "</a></label>";
+	
+	var selections = $("#"+ articleid +"_form :input[type='checkbox']:checked").length;
+	
+	if (selections == 0) {
+	    $(element).html("<label class='checkbox alert alert-warning'>No links selected</label>");
+	} else {
+	    $("#"+ articleid +"_form :input[type='checkbox']:checked").each( 
+		function(){
+		if ($(this).attr('name') != 'csrfmiddlewaretoken') {
+			var label = $("<label class='checkbox alert alert-info'>");
+			var closeButton = $("<button type='button' target='"+ $(this).attr('id') +"' class='close' data-dismiss='alert'>");
+			closeButton.html("&times;");
+			
+			closeButton.click(function(){
+				var target = closeButton.attr('target');
+				
+				console.debug("Removing 'checked' from "+target);
+				$("#"+target).removeAttr("checked");
+				console.debug("Toggling alert-success on parent of "+target);
+				$("#"+target).parent().toggleClass('alert-success');
+				console.debug("Toggling alert-warning on parent of "+target);
+				$("#"+target).parent().toggleClass('alert-warning');
+
+				console.debug("done");
+			});
+			
+			label.append(closeButton);
+			
+			$(this).siblings().clone().appendTo(label);
+			
+			
+			$(element).append(label);
+		    // $(element).append(start + $(this).attr('web') + middle1 + $(this).attr('show') + end);
+		}
+	    });	
+	}    
+}
