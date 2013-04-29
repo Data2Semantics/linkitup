825df788945f1778895bf59667256c5312e8de1a
Rinke Hoekstra <rjhoekstr@gmail.com>
2013-04-29 16:59:09 +0200

    Removed unique flag from nickname in user db

--- src/app/models.py
+++ src/app/models.py
@@ -5,7 +5,7 @@ ROLE_ADMIN = 1
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    nickname = db.Column(db.String(64), unique = True)
+    nickname = db.Column(db.String(64))
     email = db.Column(db.String(120), index = True, unique = True)
     role = db.Column(db.SmallInteger, default = ROLE_USER)
     
