41f631c2f793ab5c732ea78a4c83421626f9f01f
Marat Charlaganov <charlaganov@gmail.com>
2013-09-30 17:34:36 +0200

    Explicitly import from wtforms

--- src/app/forms.py
+++ src/app/forms.py
@@ -1,5 +1,6 @@
-from flask.ext.wtf import Form, TextField, BooleanField
-from flask.ext.wtf import Required
+from flask.ext.wtf import Form
+from wtforms import TextField, BooleanField
+from wtforms.validators import Required
 
 class LoginForm(Form):
     openid = TextField('openid', validators = [Required()])
