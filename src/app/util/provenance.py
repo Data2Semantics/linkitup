4549b4d9c960a8313c602bf103c558abf3e8bf4d
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2013-11-12 15:16:50 +0100

    Provenance decorator

--- src/app/util/provenance.py
+++ src/app/util/provenance.py
@@ -17,6 +17,86 @@ LU = Namespace('http://linkitup.data2semantics.org/resource/')
 FOAF = Namespace('http://xmlns.com/foaf/0.1/')
 
 
+def provenance(inputs=['article','inputs'],outputs=[('original','uri','show')]):
+	"""
+	This function creates a provenance decorator. 
+	
+	-- inputs:	list of keywords that should be found in the **kwargs dictionary 
+				of the function that's being decorated
+	-- outputs: list of tuples of (ancestor, id, label) keywords that should be found 
+				in the list of dictionaries that's returned by the decorated function.
+	
+	NOTE: provenance extraction only works if 
+		  * the inputs for the decorated function 
+		  * the decorated function returns a *list of dictionaries*
+		  * the decorator is used with brackets, i.e. @provenance() !!
+	
+	"""
+	app.logger.debug("Provenance: I create a provenance decorator, my arguments were:")
+	app.logger.debug(inputs)
+	app.logger.debug(outputs)
+	def provenance_decorator(func):
+		app.logger.debug("Provenance: I am the provenance_decorator, and will make a provenance wrapper around the function")
+		def provenance_wrapper(*args, **kwargs): #1
+			# print "Provenance: I am the provenance wrapper function, arguments were: %s, %s" % (args, kwargs)
+			
+			input_values = []
+			for key in inputs:
+				if key in kwargs:
+					if isinstance(kwargs[key],list) :
+						input_values.extend(kwargs[key])
+					else :
+						input_values.append(kwargs[key])
+			
+			
+			# print "\nProvenance Inputs: ", input_values
+			# print "\nProvenance Expected Outputs: ", [key for key in outputs]
+			
+			before = datetime.utcnow().isoformat()
+			out = func(*args, **kwargs)
+			after = datetime.utcnow().isoformat()
+			
+			app.logger.debug("Provenance: After the function ran")
+			
+			if not isinstance(out, dict):
+				app.logger.error("Provenance: ERROR I need a dictionary!")
+				return
+			
+			### Building the provenance dictionary
+			provenance = {}
+			
+			function_id = '{}-{}-{}'.format(func.__module__, func.__name__, func.__hash__())
+			function_label = '{}.{}'.format(func.__module__, func.__name__)
+			
+			provenance_activity = {'id': function_id, 'label': function_label, 'description': func.__doc__, 'start': before, 'end': after}
+			
+			provenance_inputs = []
+			for i in input_values:
+				provenance_inputs.append({'id': i['id'], 'label': i['label'], 'timestamp': before})
+										 
+										 
+			
+			provenance_outputs = []
+
+			app.logger.debug("Provenance: Extracted provenance")
+			prov_enhanced_output = {}
+			for key, dictionary in out.items():
+				provenance_outputs.extend([{'id': dictionary[identifier], 'label': dictionary[label], 'timestamp': after, 'ancestor': dictionary[ancestor], 'ancestor_timestamp': before } for ancestor,identifier,label in outputs if ancestor in dictionary and identifier in dictionary and label in dictionary ])
+				dictionary['ancestor_timestamp'] = before
+				dictionary['timestamp'] = after
+				prov_enhanced_output[key] = dictionary
+					
+				
+			provenance = {'activity': provenance_activity, 'inputs': provenance_inputs, 'outputs': provenance_outputs}
+
+			output = {'result': prov_enhanced_output, 'provenance': provenance}
+
+			return output
+
+		provenance_wrapper.__name__ = "{}_provenance_wrapper".format(func.__name__)
+		
+		return provenance_wrapper
+	return provenance_decorator
 def register(plugin_slug):
 	safe_slug = urllib.quote_plus(plugin_slug)
 	
