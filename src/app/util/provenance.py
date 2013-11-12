ed471e68e73c7fb067b8c3b9cfcfe041851ef362
Rinke Hoekstra <rinke.hoekstra@vu.nl>
2013-11-12 15:17:19 +0100

    Provenance trail to RDF

--- src/app/util/provenance.py
+++ src/app/util/provenance.py
@@ -104,11 +104,17 @@ def register(plugin_slug):
 	
 class Activity(object):
 	
-	def __init__(self, name):
-		# Get a UTC timestamp in ISO 8601 format
-		now = datetime.utcnow().isoformat()
+	def __init__(self, id, name=None, description=None, now=None, graph=Graph()):
+		if not now:
+			# Get a UTC timestamp in ISO 8601 format
+			now = datetime.utcnow().isoformat()
+		
+		id = unicode(id)
+		
+		if not name:
+			name = id
 
-		graph = self.graph = Graph()
+		self.graph = graph
 		
 		graph.bind('prov', PROV)
 		graph.bind('luv',LUV)
@@ -125,7 +131,10 @@ class Activity(object):
 		graph.add((self.activity_uri, PROV['startedAtTime'], Literal(now,datatype=XSD['dateTime'])))
 		graph.add((self.activity_uri, PROV['wasInfluencedBy'], URIRef('http://linkitup.data2semantics.org')))
 		graph.add((self.activity_uri, PROV['wasStartedBy'], self.user_uri))
-		graph.add((self.activity_uri, RDFS['label'], Literal(name)))
+		if name:
+			graph.add((self.activity_uri, RDFS['label'], Literal(name)))
+		if description :
+			graph.add((self.activity_uri, RDFS['comment'], Literal(description)))
 		
 		graph.add((self.user_uri, RDF.type, PROV['Person']))
 		graph.add((self.user_uri, RDF.type, PROV['Agent']))
@@ -134,40 +143,49 @@ class Activity(object):
 		graph.add((URIRef('http://linkitup.data2semantics.org'), RDF.type, PROV['Agent']))
 		
 	
-	def done(self):
-		# Get a UTC timestamp in ISO 8601 format
-		now = datetime.utcnow().isoformat()
+	def done(self, now=None):
+		if not now:
+			# Get a UTC timestamp in ISO 8601 format
+			now = datetime.utcnow().isoformat()
 
 		self.graph.add((self.activity_uri, PROV['endedAtTime'], Literal(now, datatype=XSD['dateTime'])))
 		
-		return self.serialize()
+		return self.graph
 		
 	def serialize(self):
 		return self.graph.serialize(format='turtle')
 	
-	def add_input(self, source, label='', resource_type='uri'):
+	def add_input(self, source, label='', resource_type='uri', now=None):
 		
-		source_entity_uri = self.new_entity(source, label)
+		source_entity_uri = self.new_entity(source, label=label, now=now)
 				
 		self.graph.add((self.activity_uri, PROV['used'], source_entity_uri))
 		
-	def add_output(self, target, label='', resource_type='uri'):
-		# Get a UTC timestamp in ISO 8601 format
-		now = datetime.utcnow().isoformat()
+	def add_output(self, target, label='', resource_type='uri', now=None, ancestor=None, ancestor_timestamp=None):
+		if not now:
+			# Get a UTC timestamp in ISO 8601 format
+			now = datetime.utcnow().isoformat()
 		
-		target_entity_uri = self.new_entity(target, label)
-		
+		target_entity_uri = self.new_entity(target, label=label, now=now)
 		self.graph.add((target_entity_uri, PROV['wasGeneratedBy'], self.activity_uri))
 		
+		if ancestor and ancestor_timestamp:
+			ancestor_entity_uri = self.new_entity(ancestor, now=ancestor_timestamp)
+			self.graph.add((target_entity_uri, PROV['wasDerivedFrom'], ancestor_entity_uri))
+		else :
+			app.logger.debug("Not linking {} to ancestor, as no ancestor was specified".format(target_entity_uri))
 		
-	def new_entity(self, uri, label):
-		# Get a UTC timestamp in ISO 8601 format
-		now = datetime.utcnow().isoformat()
+		
+	def new_entity(self, uri, label=None, now=None):
+		if not now:
+			# Get a UTC timestamp in ISO 8601 format
+			now = datetime.utcnow().isoformat()
+		
+		uri = unicode(uri)
 		
 		if uri.startswith('http://') :
 			resource_uri = URIRef(uri)
 		else :
-			
 			resource_uri = LU[get_qname(uri)]
 			
 		resource_entity_uri = LU[urllib.quote_plus("entity/{}_{}".format(label, now), safe="/")]
@@ -175,7 +193,11 @@ class Activity(object):
 		self.graph.add((resource_uri, RDFS['label'], Literal(label)))
 		
 		self.graph.add((resource_entity_uri, RDF.type, PROV['Entity']))
-		self.graph.add((resource_entity_uri, RDFS['label'], Literal(label)))
+		
+		if label:
+			self.graph.add((resource_entity_uri, RDFS['label'], Literal(label)))
+		else :
+			self.graph.add((resource_entity_uri, RDFS['label'], Literal(uri)))
 		self.graph.add((resource_entity_uri, PROV['wasDerivedFrom'], resource_uri))
 		self.graph.add((resource_entity_uri, PROV['wasGeneratedAt'], Literal(now, datatype=XSD['dateTime'])))
 		
