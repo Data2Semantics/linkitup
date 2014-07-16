f7c08d2375436456c8db8fa58735396cc4b35950
Marat Charlaganov <charlaganov@gmail.com>
2014-07-16 16:02:40 +0200

    Clean up

--- setup.py
+++ setup.py
@@ -11,32 +11,6 @@ setup(name='Linkitup',
       url='http://github.com/Data2Semantics/linkitup',
       package_data={'app':['templates/*.html','templates/*.query','templates/*.js','static/js/*.js','static/css/*.css','static/css/*.png','static/img/*.png','static/js/vendor/*.js']},
       packages=find_packages(),
-      scripts=['manage.py','run.py'],
-      long_description=open('README.txt').read(),
+      scripts=['manage.py'],
+      # long_description=open('README.txt').read(),
       install_requires=requirements)
-
-
-# import os, subprocess, sys
-# 
-# subprocess.call(['pip', 'install', 'flask'])
-# subprocess.call(['pip', 'install', 'flask-login'])
-# subprocess.call(['pip', 'install', 'flask-openid'])
-# if sys.platform == 'win32':
-#     subprocess.call(['pip', 'install', '--no-deps', 'lamson', 'chardet', 'flask-mail'])
-# else:
-#     subprocess.call(['pip', 'install', 'flask-mail'])
-# subprocess.call(['pip', 'install', 'sqlalchemy==0.7.9'])
-# subprocess.call(['pip', 'install', 'flask-sqlalchemy'])
-# subprocess.call(['pip', 'install', 'sqlalchemy-migrate'])
-# subprocess.call(['pip', 'install', 'flask-whooshalchemy'])
-# subprocess.call(['pip', 'install', 'flask-wtf'])
-# subprocess.call(['pip', 'install', 'flask-babel'])
-# subprocess.call(['pip', 'install', 'flup'])
-# # subprocess.call(['pip', 'install', 'flask-kvsession'])
-# subprocess.call(['pip', 'install', 'flask-uploads'])
-# subprocess.call(['pip', 'install', 'rdflib', '--upgrade'])
-# subprocess.call(['pip', 'install', 'SPARQLWrapper'])
-# subprocess.call(['pip', 'install', 'beautifulsoup4'])
-# subprocess.call(['pip', 'install', 'pdfminer'])
-# subprocess.call(['pip', 'install', 'requests'])
-
