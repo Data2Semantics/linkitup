c3ec6f61bd22cf7c6a64f4f2102d8f79ea9e2e4f
Marat Charlaganov <charlaganov@gmail.com>
2014-09-09 16:38:33 +0200

    More respect to PEP-8

--- linkitup/spotlight/plugin.py
+++ linkitup/spotlight/plugin.py
@@ -17,35 +17,35 @@ SPOTLIGHT_URL = 'http://spotlight.dbpedia.org/rest/annotate'
 SPOTLIGHT_CONFIDENCE = 0.2
 SIMILARITY_CUTOUT = 0.1
 
+
 @app.route('/spotlight', methods=['POST'])
 @login_required
-@plugin(fields=[('tags','id','name'),('categories','id','name')], link='link')
+@plugin(fields=[('tags', 'id', 'name'), ('categories', 'id', 'name')],
+        link='link')
 @provenance()
 def link_to_spotlight(*args, **kwargs):
 
     article_id = kwargs['article']['id']
 
-    app.logger.debug("Running DBpedia Spotlight plugin for article {}".format(article_id))
+    app.logger.debug("Running DBpedia Spotlight plugin for article {}".format(
+        article_id))
 
-    match_items = kwargs['inputs']
-    labels = ", ".join([item['label'] for item in kwargs['inputs'] + [kwargs['article']]])
-    
+    labels = ", ".join([item['label'] for item in kwargs['inputs']
+                        + [kwargs['article']]])
+
     # Get article description, the wrapper does not provide it yet
     article = request.get_json()
     soup = BeautifulSoup(article['description'])
     description = soup.get_text()
 
     # Query text consists of article title, description, tags, and categories
-    # app.logger.debug("Spotlight query text:\n {}".format(text))
-
-    response = requests.post(   SPOTLIGHT_URL,
-                                data={'text': text, 'confidence': SPOTLIGHT_CONFIDENCE},
-                                headers={'Accept': 'application/json'})
     text = u"{}\n\n{}".format(description, labels)
+    response = requests.post(
+        SPOTLIGHT_URL,
+        data={'text': text, 'confidence': SPOTLIGHT_CONFIDENCE},
+        headers={'Accept': 'application/json'})
     resources = response.json().get('Resources', [])
-
     matches = {}
-
     for resource in resources:
         dbpedia_uri = resource['@URI']
         similarity_score = float(resource['@similarityScore'])
@@ -64,9 +64,5 @@ def link_to_spotlight(*args, **kwargs):
                  'extra': types,
                  'subscript': score,
                  'original': article_id}
-        
-            # Append it to all matches
         matches[dbpedia_uri] = match
-
-    # Return the matches
     return matches
