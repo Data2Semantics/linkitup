cad40c5f74ba8972ef30fce2cbf0c6f4bd87441d
Marat Charlaganov <charlaganov@gmail.com>
2014-10-30 17:23:22 +0100

    Fix S4 plugin

--- linkitup/s4/plugin.py
+++ linkitup/s4/plugin.py
@@ -4,16 +4,15 @@ Created on 1 Oct 2014
 @author: RinkeHoekstra
 '''
 
+import json
 from flask import request
 from flask.ext.login import login_required
 import requests
 from bs4 import BeautifulSoup
 
 from linkitup import app
-from linkitup.util import wikipedia_url
 from linkitup.util.baseplugin import plugin
 from linkitup.util.provenance import provenance
-from linkitup.util.preflabel import label
 
 
 @app.route('/s4', methods=['POST'])
@@ -38,37 +37,45 @@ def link_to_s4(*args, **kwargs):
 
     # Query text consists of article title, description, tags, and categories
     text = u"{}\n\n{}".format(description, labels)
-    
     API_KEY = "s4io0o6gv3df"
     SECRET = "n5ko8i0j4ilb2dv"
 
     ontotext_s4_url = "https://text.s4.ontotext.com/v1/sbt"
 
-    headers = {'Content-Type': 'application/json', 'Accept': 'application/json', 'Accept-Encoding': 'gzip'}
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Accept-Encoding': 'gzip'}
 
     data = {
-       'document' : text,
-       'documentType' : 'text/plain',
-       'annotationSelectors' : []
-    }
+        'document': text,
+        'documentType': 'text/plain',
+        'annotationSelectors': []}
 
-    r = requests.post(ontotext_s4_url, data=json.dumps(data), headers=headers, auth=(API_KEY,SECRET))
+    r = requests.post(
+        ontotext_s4_url,
+        data=json.dumps(data),
+        headers=headers,
+        auth=(API_KEY, SECRET))
 
-    resources = response.json().get('entities',[])
-    
-    matches {}
+    resources = r.json().get('entities', [])
+    matches = {}
     for s4_name, resource in resources.items():
-        s4_uri = resource[0]['inst']
-        s4_types = [resource[0]['class']]
-        
-        match = {'type': 'link',
-            'uri': s4_uri,
-            'web': s4_uri,
-            'show': s4_name,
-            'extra': s4_types,
-            'original': article_id
-        }
-        
-        matches[s4_uri] = match
-    
+        try:
+            s4_uri = resource[0]['inst']
+            s4_types = [resource[0]['class']]
+        except KeyError as e:
+            app.logger.error("Key error '{}' in {}".format(
+                e.message, resource))
+        else:
+            match = {
+                'type': 'link',
+                'uri': s4_uri,
+                'web': s4_uri,
+                'show': s4_name,
+                'extra': s4_types,
+                'original': article_id
+            }
+            matches[s4_uri] = match
+
     return matches
